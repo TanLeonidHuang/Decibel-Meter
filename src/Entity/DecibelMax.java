c1c4ec8085f431a02e258cfaba8d021d247a94e8
huangn12 <nathaniel.huang@mail.utoronto.ca>
2022-08-25 14:26:39 -0700

    Added DcibelMax entity. Stores the maxDB that the user inputted.

---
+++ src/Entity/DecibelMax.java
@@ -0,0 +1,19 @@
+package Entity;
+public class DecibelMax {
+    private int DBMax;
+
+    /**
+     * 
+     */
+    public DecibelMax() {
+        this.DBMax = 2;
+    }
+
+    public int getDBMax() {
+        return DBMax;
+    }
+
+    public void setDBMax(int DBMax) {
+        this.DBMax = DBMax;
+    }
+}
