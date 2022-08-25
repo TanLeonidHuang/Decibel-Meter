1ece843e8530c3717857af320b4f3652cace70c1
huangn12 <nathaniel.huang@mail.utoronto.ca>
2022-08-25 14:28:20 -0700

    Runs the program

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import Controller.DecibelMaxController;
+import Entity.Sound;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Sound recorder = new Sound();
+        DecibelMaxController controller = new DecibelMaxController();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter DB max: ");
+        int userInput = scanner.nextInt();
+        controller.userInput(userInput);
+        final Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                recorder.run(controller);
+                Runtime.getRuntime().addShutdownHook(new Thread() {
+                    @Override
+                    public void run() {
+                        recorder.close();
+                    }
+                });
+            }
+        };
+        new Thread(r).start();
+    }
+}
+
